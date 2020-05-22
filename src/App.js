7cd42b06c11d58c0adf80b46969f5d6013ba95e6
bkorumaz <bahadir.korumaz@gmail.com>
2020-05-22 05:00:46 +0300

    finished working example and added errorous character color feature

--- src/App.js
+++ src/App.js
@@ -1,18 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import { generate } from "./utils/words";
 import useKeyPress from "./hooks/useKeyPress";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  
   useKeyPress((key) => {
-    console.log(key);
+    let updatedOutgoingChars = outgoingChars;
+    let updatedIncomingChars = incomingChars;
+
+    if (key.toLowerCase() === currentChar) {
+      setWrongCharacter(false);
+      if (leftPadding.length > 0) {
+        setLeftPadding(leftPadding.substring(1));
+      }
+
+      updatedOutgoingChars += currentChar;
+      setOutgoingChars(updatedOutgoingChars);
+
+      setCurrentChar(incomingChars.charAt(0));
+
+      updatedIncomingChars = incomingChars.substring(1);
+      if (updatedIncomingChars.split(" ").length < 10) {
+        updatedIncomingChars += " " + generate();
+      }
+      setIncomingChars(updatedIncomingChars);
+    } else {
+      setWrongCharacter(true);
+    }
   });
 
   const initialWords = generate();
 
+  const [leftPadding, setLeftPadding] = useState(
+    new Array(20).fill(" ").join("")
+  );
+  const [wrongCharacter, setWrongCharacter] = useState(false);
+  const [outgoingChars, setOutgoingChars] = useState("");
+  const [currentChar, setCurrentChar] = useState(initialWords.charAt(0));
+  const [incomingChars, setIncomingChars] = useState(initialWords.substr(1));
+
   return (
     <>
-      <div>Generated words: {initialWords}</div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p className="Character">
+            <span className="Character-out">
+              {(leftPadding + outgoingChars).slice(-20)}
+            </span>
+            <span className={`Character-current${wrongCharacter ? "-error" : ""}`}>{currentChar}</span>
+            <span>{incomingChars.substr(0, 20)}</span>
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
     </>
   );
 }
